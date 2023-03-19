20b38029b5ccc11e100142bfdf11b65ae564bb2c
Максим Рудой <doodoo690@gmail.com>
2023-03-19 21:41:17 +0500

    finish index

--- source/js/app.js
+++ source/js/app.js
@@ -1,35 +1,15 @@
-const headerBtn = document.querySelector('.header__button');
-const bodyMenu = document.querySelector('.page__body');
-const menuNavDropDown = document.querySelector('.menu');
-const menuNavDropDownTwo = document.querySelector('.menu__user');
-
-headerBtn.addEventListener('click', () => {
-  const exp = 'true' === headerBtn.getAttribute('aria-expanded')
-  headerBtn.setAttribute('aria-expanded', !exp)
+const header = document.querySelector('.header');
+const headerButton = document.querySelector('.header__button');
 
 
-headerBtn.classList.toggle('header__button--active')
-bodyMenu.classList.toggle('page__body--menu')
-menuNavDropDown.classList.toggle('menu--drop-list')
-menuNavDropDownTwo.classList.toggle('menu--drop-list')
 
-exp ? headerBtn.setAttribute('aria-label', 'Открыть меню')
-: headerBtn.setAttribute('aria-label', 'Закрыть меню')});
-
-document.addEventListener('DOMContentLoaded', function (Event) {
-
-  const  menuNoJs = document.querySelector('.menu')
-  const  headerButton = document.querySelector('.header__button')
-  const  headerNoJs = document.querySelector('.menu')
-  menuNoJs.classList.remove('menu--no-js'),
-  headerButton.classList.remove('header__button--no-js')
-  headerNoJs.classList.remove('menu--no-js')});
+header.classList.remove('no-js');
+headerButton.addEventListener('click', () =>
+  header.classList.toggle('is-open'));
 
 document.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape') {
-      headerNavDropDown.classList.remove('menu--drop-list');
-      bodyMenu.classList.remove('page__body--menu')
-      headerBtn.classList.remove('header__button--active')
-    }
+  if (evt.key === 'Escape') {
+    header.classList.remove('is-open');
   }
-  );
+}
+);
