67c69a182cc1c0c963eec5f6331e17e326ac7b22
Максим Рудой <doodoo690@gmail.com>
2023-03-30 22:28:38 +0500

    add new fix

--- source/js/app.js
+++ source/js/app.js
@@ -18,27 +18,44 @@ document.addEventListener('keydown', (evt) => {
 
 // modal open window
 
-const modalButtons = document.querySelectorAll('.modal__open');
+  const modalButtons = document.querySelectorAll('.modal__open');
   const modal = document.querySelector('.modal');
   const modalCloseButton = document.querySelector('.modal__submit-button');
+  const modalFormInput = document.querySelector('.modal__button-input');
 
   const onModalButtonClick = (evt) => {
     evt.preventDefault();
     modal.classList.add('modal-open');
+    modalFormInput.focus();
   };
 
-  modalButtons.forEach((button) => button.addEventListener('click', onModalButtonClick));
+  const onModalCloseButtonClick = (evt) => {
+    evt.preventDefault();
+    modal.classList.remove('modal-open');
+  };
 
   modalCloseButton.addEventListener('click', () => {
     evt.preventDefault();
     modal.classList.remove('modal-open');
   });
 
-  // close modal window on push keydown Escape
-
-  document.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape') {
-      evt.preventDefault();
-      modal.classList.remove('modal-open');
+  const onModalClick = (evt) => {
+    if (evt.target === modal) {
+      modal.classList.remove("modal-open");
     }
-  });
+  };
+
+const onDocumentKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    modal.classList.remove('modal-open');
+  }
+};
+
+
+
+
+  modalButtons.forEach((button) => button.addEventListener('click', onModalButtonClick));
+  modalCloseButton.addEventListener('click', onModalCloseButtonClick);
+  modal.addEventListener('click', onModalClick);
+  document.addEventListener('keydown', onDocumentKeydown);
